31ab329cabde58d1d3f963f619d82b94d21e8fcf
useur <useur@ONIZUKA-PC>
2021-04-29 23:16:07 +0000

    First commit

---
+++ backend/polls/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+    
+class Categories(models.Model):
+    Nom_cat=models.CharField(max_length=20)
+    Desc_cat=models.CharField(max_length=100)
+    
+class Gamme(models.Model):
+    Nom_gam=models.CharField(max_length=20)
+    Desc_gam=models.CharField(max_length=100)
+
+
+class Product(models.Model):
+    Nom_prod=models.CharField(max_length=20)
+    Prix=models.IntegerField()
+    #img=models.ImageField(upload_to='')
+    Desc_prod=models.CharField(max_length=100)
+    nbr_stock=models.IntegerField()
+    id_cat=models.ForeignKey(Categories,on_delete=models.CASCADE)
+    id_ga=models.ForeignKey(Gamme,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Nom
+    
