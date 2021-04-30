be4c91de5c2c17a72eb4d86e037f3d33b6f6501e
Asffar hassan <74249279+Asffar-Hassan@users.noreply.github.com>
2021-04-30 06:05:52 +0200

    Update views.py

--- backend/polls/views.py
+++ backend/polls/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from .models import Categories,Gamme,Product
 
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    products=Product.objects.all()
+    list_prod={
+        'products':products
+    }
+
+    return render(request,'index-shop.html',list_prod)
 
 def about(request):
     return render(request,'about.html')
